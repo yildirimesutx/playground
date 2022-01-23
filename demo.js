f64ddfbbbc6354e7b25e175b3022bbb6e8bc8aaa
mesut <yildirimesutcw@gmail.com>
2022-01-24 01:21:01 +0300

    operators end

--- demo.js
+++ demo.js
@@ -27,7 +27,7 @@ screen */
 
 // a=5;
 
-// console.log(a+b);
+// console.log(a+b                                                                                                                                                                                            );
 
 // const a=4;
 
@@ -37,16 +37,95 @@ screen */
 
 // console.log(a+b);
 
-console.log(typeof 0);
-console.log(typeof "game")
-console.log(typeof NaN);
+// console.log(typeof 0);
+// console.log(typeof "game")
+// console.log(typeof NaN);
 
-console.log(typeof (1/0));
-console.log(typeof (0/1));
-console.log(typeof null)
+// console.log(typeof (1/0));
+// console.log(typeof (0/1));
+// console.log(typeof null)
 
-var x = 0.2 + 0.2;
-console.log(x);
-Boolean(2+3);
+// var x = 0.2 + 0.2;
+// console.log(x);
+// Boolean(2+3);
+
+// let x = 3;
+// const y = x++ ;
+
+// console.log({x},{y});
+
+// let a = 3;
+// const b = ++a;
+
+// console.log({a},{b});
+
+// var a = "3";
+// var b= 6;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a/b);
+
+// var a = 10;
+
+// console.log(5 + a++);
+// console.log(a);
+
+// var a = 12;
+
+// var b= 8 ;
+
+// console.log(a+ ++b);
+
+// var c = 8;
+
+// var d = c++;
+
+// var e = ++d;
+
+// console.log(d);
+
+// console.log(e);
+
+// var a = 5;
+
+
+// console.log(a++ + ++a);
+
+// var input = prompt("enter your name");
+
+// console.log(input);
+
+
+// var a = 4;
+// var b = "4";
+
+// console.log(a==b);
+
+// console.log(a===b);
+
+// var x =null;
+
+// var y = undefined;
+
+// console.log(x==y);
+
+// console.log(x===y);
+
+// console.log(null && 0);
+
+// var a = (2 || 3);
+
+// console.log(a);
+
+// var capitalLetter = false;
+// var symbol = true;
+// var passLength = true;
+
+var result = capitalLetter && symbol || passLength;
+
+console.log(!result || 'Try again');
+
+
 
 
