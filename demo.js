8158952e981b999522a5b0f9aec34d50d30abafa
mesut <yildirimesutcw@gmail.com>
2022-01-23 23:13:31 +0300

    demo

--- demo.js
+++ demo.js
@@ -1,2 +1,52 @@
 /* ScreenOrientation
 screen */
+
+// window.alert("Naber");
+// document.write("hello world");
+// console.log("Hello word");
+
+// let a=3;
+// let b=4;
+
+// console.log(a+b);
+
+// a=b;
+
+// console.log(a+b)
+
+// var a= 5;
+
+// a+= 6;
+
+// var b = 6;
+
+// console.log(a+b);
+
+// let a = 4;
+// let b = 3;
+
+// a=5;
+
+// console.log(a+b);
+
+// const a=4;
+
+// const b=5;
+
+// a=6;
+
+// console.log(a+b);
+
+console.log(typeof 0);
+console.log(typeof "game")
+console.log(typeof NaN);
+
+console.log(typeof (1/0));
+console.log(typeof (0/1));
+console.log(typeof null)
+
+var x = 0.2 + 0.2;
+console.log(x);
+Boolean(2+3);
+
+
